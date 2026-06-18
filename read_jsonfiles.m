5edd492ce68e082b4b81cd05aece2835ab4759f2
Wei Chen <107769478+Evildes000@users.noreply.github.com>
2026-06-18 15:55:15 +0800

    Add files via upload

---
+++ read_jsonfiles.m
@@ -0,0 +1,8 @@
+clc
+clear all
+close all
+
+
+path_to_setupjsonfile = "C:\ti\mmwave_studio_03_01_04_04\mmWaveStudio\PostProc\data_17_06_1920.setup.json";
+MyRawDataReader(path_to_setupjsonfile)
+% RealTimeRangeAngle(path_to_setupjsonfile)
